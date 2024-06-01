1c12691d4afbc18b0e25afb9bb586d03a8d025c5
MaddoScientisto <maddo.science@gmail.com>
2024-06-01 17:22:51 +0200

    Added service worker

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,14 @@
 import 'babel-polyfill/dist/polyfill';
 import '../scss/index.scss';
 import initApp from './app/initApp';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 document.addEventListener('DOMContentLoaded', () => {
   initApp();
 });
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
